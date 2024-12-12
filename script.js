ce8264a0967d0afdd2244f010a1e022da63f1f9a
PhamNghia <huunghia25012002@gmail.com>
2024-12-12 15:35:50 +0700

    push

--- script.js
+++ script.js
@@ -14,13 +14,11 @@ document.addEventListener("DOMContentLoaded", () => {
   const cursorElement = document.getElementById("cursor");
   const typedTextElement = document.getElementById("typed-text");
 
-  // Kiểm tra nếu phần tử tồn tại
   if (cursorElement) {
     let cursor = true;
     const speed = 100;
 
     setInterval(() => {
-      // Thay đổi opacity của #cursor
       cursorElement.style.opacity = cursor ? 1 : 0;
       cursor = !cursor;
     }, speed);
@@ -28,7 +26,6 @@ document.addEventListener("DOMContentLoaded", () => {
     console.error("Element with id 'cursor' not found.");
   }
 
-  // Kiểm tra #typed-text nếu cần thiết
   if (typedTextElement) {
     console.log("typed-text element found!");
   }
@@ -36,42 +33,38 @@ document.addEventListener("DOMContentLoaded", () => {
 
 document.addEventListener("DOMContentLoaded", () => {
   const sections = document.querySelectorAll("section");
-  let lastScrollY = window.scrollY; // Lưu vị trí cuộn trước
+  let lastScrollY = window.scrollY;
 
   const observer = new IntersectionObserver(
     (entries, observer) => {
       entries.forEach((entry) => {
         if (entry.isIntersecting) {
           if (window.scrollY > lastScrollY) {
-            // Khi cuộn xuống (scroll down)
-            entry.target.classList.add("show"); // Hiện phần tử từ bên trái hoặc bên phải
+            entry.target.classList.add("show");
             entry.target.classList.remove("hidden_left");
             entry.target.classList.remove("hidden_right");
           } else {
-            // Khi cuộn lên (scroll up)
-            entry.target.classList.add("show"); // Hiện phần tử từ bên trái hoặc bên phải
+            entry.target.classList.add("show");
             entry.target.classList.remove("hidden_left");
             entry.target.classList.remove("hidden_right");
           }
         } else {
-          // Khi phần tử ra khỏi màn hình, ẩn lại
           entry.target.classList.remove("show");
-          entry.target.classList.add("hidden_right"); // Ẩn từ bên phải hoặc bên trái
+          entry.target.classList.add("hidden_right");
         }
       });
     },
     {
-      threshold: 0.1, // Khi 10% phần tử vào màn hình
+      threshold: 0.1,
     }
   );
 
   sections.forEach((section) => {
-    section.classList.add("hidden_right"); // Ẩn phần tử ban đầu
+    section.classList.add("hidden_right");
     section.classList.add("hidden_left");
-    observer.observe(section); // Quan sát các phần tử
+    observer.observe(section);
   });
 
-  // Cập nhật vị trí cuộn mỗi khi người dùng cuộn trang
   window.addEventListener("scroll", () => {
     lastScrollY = window.scrollY;
   });
@@ -80,15 +73,15 @@ function waitForNameToShow() {
   return new Promise((resolve) => {
     const interval = setInterval(() => {
       if (hasShownName) {
-        clearInterval(interval); // Dừng kiểm tra
-        resolve(); // Kết thúc Promise
+        clearInterval(interval);
+        resolve();
       }
-    }, 50); // Kiểm tra mỗi 50ms
+    }, 50);
   });
 }
 
 async function startTypingEffect() {
-  await waitForNameToShow(); // Đợi đến khi `hasShownName = true`
+  await waitForNameToShow();
   const typedText = document.getElementById("typed-text");
   const cursor = document.getElementById("cursor");
 
@@ -134,7 +127,7 @@ async function startTypingEffect() {
   let currentIndex = 0;
   let currentLetterIndex = 0;
   let isDeleting = false;
-  let typingSpeed = 50; // Adjust typing speed
+  let typingSpeed = 50;
 
   function type() {
     const { word, slogan } = wordsWithSlogans[currentIndex];
@@ -172,15 +165,14 @@ async function startTypingEffect() {
 }
 document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
   anchor.addEventListener("click", function (e) {
-    e.preventDefault(); // Ngừng hành động mặc định
+    e.preventDefault();
 
-    // Cuộn mượt đến phần tử đích
     document.querySelector(this.getAttribute("href")).scrollIntoView({
-      behavior: "smooth", // Tạo hiệu ứng cuộn mượt
+      behavior: "smooth",
     });
   });
 });
-// Lấy phần tử image-popup và popup-image
+
 const imagePopup = document.getElementById("image-popup");
 const popupImage = document.getElementById("popup-image");
 
@@ -235,24 +227,21 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 document.addEventListener("DOMContentLoaded", function () {
-  // Cấu hình IntersectionObserver
   var options = {
     root: null,
     rootMargin: "0px",
-    threshold: 0.5, // Khi phần tử xuất hiện 50%
+    threshold: 0.5,
   };
 
   var observer = new IntersectionObserver(function (entries) {
     entries.forEach(function (entry) {
       if (entry.isIntersecting) {
         setTimeout(function () {
-          const bgImage = entry.target.dataset.bgImage; // Lấy giá trị từ data-bg-image
+          const bgImage = entry.target.dataset.bgImage;
           if (bgImage) {
             if (bgImage.startsWith("#")) {
-              // Nếu là mã màu
               entry.target.style.backgroundColor = bgImage;
             } else {
-              // Nếu là URL hình ảnh
               entry.target.style.backgroundImage = "url(" + bgImage + ")";
               entry.target.style.backgroundSize = "cover";
               entry.target.style.backgroundPosition = "center";
@@ -261,24 +250,23 @@ document.addEventListener("DOMContentLoaded", function () {
           entry.target.style.transition =
             "opacity 0.5s ease, background 0.5s ease";
           entry.target.style.opacity = "1";
-        }, 500); // Sau 1.3 giây
+        }, 500);
       } else {
         setTimeout(function () {
           entry.target.style.opacity = "0";
           entry.target.style.transition = "opacity 0.5s ease";
-        }, 500); // Sau 1.3 giây
+        }, 500);
       }
     });
   }, options);
 
-  // Quan sát các phần tử có class "page"
   document.querySelectorAll(".page").forEach(function (section) {
     observer.observe(section);
   });
 });
 function animateCounter(counter) {
   const target = +counter.getAttribute("data-count");
-  const step = target / 100; // Increment step
+  const step = target / 100;
   let count = 0;
 
   const updateCounter = () => {
@@ -294,12 +282,10 @@ function animateCounter(counter) {
   updateCounter();
 }
 
-// Reset counters to 0
 function resetCounter(counter) {
   counter.textContent = 0;
 }
 
-// Observe when the section is visible
 const observer = new IntersectionObserver(
   (entries) => {
     entries.forEach((entry) => {
@@ -307,7 +293,7 @@ const observer = new IntersectionObserver(
       if (entry.isIntersecting) {
         setTimeout(() => {
           counters.forEach((counter) => animateCounter(counter));
-        }, 1500); // Delay 1.5 seconds
+        }, 1500);
       } else {
         counters.forEach((counter) => resetCounter(counter));
       }
@@ -316,50 +302,42 @@ const observer = new IntersectionObserver(
   { threshold: 0.5 }
 );
 
-// Attach observer to the objective section
 const objectiveSection = document.querySelector("#objective");
 observer.observe(objectiveSection);
 document.addEventListener("DOMContentLoaded", () => {
   const experienceSection = document.querySelector("#experience");
-  const hiddenElements = experienceSection.querySelectorAll(".hidden"); // Tất cả các phần tử có class 'hidden'
-
+  const hiddenElements = experienceSection.querySelectorAll(".hidden");
   const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
       const target = entry.target;
 
       if (entry.isIntersecting) {
-        // Hiện phần tử sau 500ms
         setTimeout(() => {
           target.classList.add("show");
           target.classList.remove("hidden");
         }, 500);
       } else {
-        // Ẩn lại nếu phần tử không còn trong viewport
         target.classList.remove("show");
         target.classList.add("hidden");
       }
     });
   });
 
-  // Quan sát tất cả các phần tử ẩn
   hiddenElements.forEach((element) => observer.observe(element));
 });
-// Lấy các phần tử
+
 const previewBtn = document.getElementById("previewBtn");
 const modal = document.getElementById("previewModal");
 const closeModal = document.getElementById("closeModal");
 
-// Mở modal khi nhấn nút xem trước
 previewBtn.addEventListener("click", () => {
   modal.style.display = "flex";
 });
 
-// Đóng modal khi nhấn vào dấu "x"
 closeModal.addEventListener("click", () => {
   modal.style.display = "none";
 });
 
-// Đóng modal khi nhấn bên ngoài modal
 window.addEventListener("click", (event) => {
   if (event.target === modal) {
     modal.style.display = "none";
